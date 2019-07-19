45c2c6ca815fe068183c2478317336bf9f330d80
Zhipeng-Chang <zchang0302@gmail.com>
2019-07-19 16:51:31 -0600

    create env

--- wordMerge/settings.py
+++ wordMerge/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0(ezvw-a0$^dz2fw^#b2*ef8%f9vna5fue=bm)l-wt#gs7%qxf'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", default="True") == "True"
 
 ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1','localhost']
 
@@ -79,11 +80,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-GOOGLE_OAUTH2_CLIENT_ID = '840492959274-c625ken52ouu76i42jr90t6e245cg6ju.apps.googleusercontent.com'
-GOOGLE_OAUTH2_CLIENT_SECRET = 'NUgiZKOmNN2bNZkvrDkVxMjo'
+GOOGLE_OAUTH2_CLIENT_ID = config("GOOGLE_OAUTH2_CLIENT_ID")
+GOOGLE_OAUTH2_CLIENT_SECRET = config("GOOGLE_OAUTH2_CLIENT_SECRET")
 
-SOCIAL_AUTH_GOOGLE_OAUTH2_KEY ='840492959274-c625ken52ouu76i42jr90t6e245cg6ju.apps.googleusercontent.com'
-SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = 'NUgiZKOmNN2bNZkvrDkVxMjo'
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY =config("SOCIAL_AUTH_GOOGLE_OAUTH2_KEY")
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config("SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET")
 ROOT_URLCONF = 'wordMerge.urls'
 
 TEMPLATES = [
