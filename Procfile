e918f26d261b9c9b729ea9e718cfc1c01999d207
Zhipeng-Chang <zchang0302@gmail.com>
2019-07-19 23:40:23 -0600

    bug fix

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 release: python3 manage.py migrate
-web: gunicorn mysite.wsgi
+web: gunicorn wordMerge.wsgi
--- wordMerge/settings.py
+++ wordMerge/settings.py
@@ -156,7 +156,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 STATIC_URL = '/static/'
+ROOT_URLCONF = 'wordMerge.urls'
