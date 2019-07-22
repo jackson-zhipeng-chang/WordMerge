f3e544848ce0f27754c27d63cf067853740555c6
Zhipeng-Chang <zchang0302@gmail.com>
2019-07-22 11:12:19 -0600

    update db

--- wordMerge/settings.py
+++ wordMerge/settings.py
@@ -113,14 +113,28 @@ TIME_ZONE = 'America/Edmonton'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+'''
+in_heroku = False
+if 'DATABASE_URL' in os.environ:
+    in_heroku = True
 
+import dj_database_url
+if in_heroku:
+    DATABASES = {'default': dj_database_url.config()}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
